37e755b57335e826c1f137e8d30dcb2b790d7cc3
Talha Ozleblebici <talha43gs@gmail.com>
2020-05-10 00:45:58 +0100

    GroupStudyProjects Update

--- src/test/java/com/vytrack/tests/components/UserStory1_TestCase.java
+++ src/test/java/com/vytrack/tests/components/UserStory1_TestCase.java
@@ -1,13 +1,10 @@
 package com.vytrack.tests.components;
 
-
-import com.vytrack.utilities.DriverLogin;
-import com.vytrack.utilities.FleetMenuTab;
-import com.vytrack.utilities.LoadingWait;
 import com.vytrack.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -40,10 +37,14 @@ public class UserStory1_TestCase {
 
     @DataProvider(name = "UserLoginPreConditions")
     public Object[][] loginData(Method testCase){    // username + password sıralamasını kullan!!
-
-
+    /* long way
+        Object [][] users = new Object[][]{
+                {"user7", "UserUser123"},
+                {"user8", "UserUser123"},
+                {"user9", "UserUser123"}
+        }; */
         return new Object[][]{
-                {"user7" , "UserUser123"}/*,
+                    {"user7" , "UserUser123"}/*,
                     {"user8" , "UserUser123"},
                     {"user9" , "UserUser123"}*/ };
     }
@@ -51,64 +52,61 @@ public class UserStory1_TestCase {
 
 
     @Test (dataProvider = "UserLoginPreConditions" )
-    public void UserStory_1_Test(String username, String password) throws InterruptedException {
-        // User Story 1: As a truck driver I should be able to access Vehicle under Fleet module.
-        // Acceptance Criteria
+    public void UserStory_1_Test(String userName, String password) throws InterruptedException {
+    // User Story 1: As a truck driver I should be able to access Vehicle under Fleet module.
+    // Acceptance Criteria
 //        1.Verify that truck driver should be able to see all Vehicle information once navigate to Vehicle page.
 //        2.Verify that when user  click on any car on the grid , system should display general information about the car
 //        3.Verify that truck driver can add Event and it should display under Activity tab and General information page as well .
 //        4.Verify that Truck driver can reset the Grid by click on Grid setting
 
         // Driver login home page
-        DriverLogin.driverLogin(driver,username,password);
-        String dashboardTitle= driver.getTitle();
+        driver.findElement(By.id("prependedInput")).sendKeys(userName);
+        driver.findElement(By.id("prependedInput2")).sendKeys(password);
+        driver.findElement(By.id("_submit")).click();
+        String actualTitle= driver.getTitle();
+
 
         // waite for loading  circle
-        LoadingWait.ExplicitWait(driver);
-
-        // click module and wait for loading  circle and verify title change
-        FleetMenuTab.clickModule(driver,"Vehicle");
-        LoadingWait.ExplicitWait(driver);
-        String moduleTitle = driver.getTitle();
-
-        Assert.assertNotEquals(dashboardTitle,moduleTitle);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement loader= driver.findElement(By.cssSelector(".loader-mask>.loader-frame"));
+        wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
 
 
         //*****************  Verify AC-1  *****************
-        String expectedTitle = "Car - Entities - System - Car - Entities - System";
-        String actualTitle = driver.getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
 
+        // click module
+        WebElement fleetModule = driver.findElement(By.xpath("//span[contains(text(), 'Fleet')]"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(fleetModule).pause(1000).perform();
+        driver.findElement(By.xpath("//span[.='Vehicles']")).click();
+
+        //wait for loading  circle
+        wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
+
+        //verify Vehicle information page is displayed
+        String expectedTitle= "Car - Entities - System - Car - Entities - System";
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+
+        //extra verify with table heads
         String expectedInfo = "LICENSE PLATE\n" + "TAGS\n" + "DRIVER\n" + "LOCATION\n" + "CHASSIS NUMBER\n" + "MODEL YEAR\n" + "LAST ODOMETER\n" + "IMMATRICULATION DATE\n" + "FIRST CONTRACT DATE\n" + "CVVI\n" + "SEATS NUMBER\n" + "DOORS NUMBER\n" + "COLOR\n" + "TRANSMISSION\n" + "FUEL TYPE\n" + "CO2 EMISSIONS\n" + "HORSEPOWER\n" + "HORSEPOWER TAXATION\n" + "POWER (KW)";
         String actualInfo =driver.findElement(By.xpath("//thead[@class='grid-header']")).getText();
-
         Assert.assertEquals(actualInfo,expectedInfo);
 
 
         //*****************  Verify AC-2  *****************
-        /* If u want to usespecific car use like tihs!! But it is not static and strong way!
-        driver.findElement(By.xpath("//td[contains(text(),'14EF6562')]")).click();
-        LoadingWait.ExplicitWait(driver);
 
-        // different way
-        String plate = "14EF6562";
-        driver.findElement(By.xpath("//td[contains(text(),'"+plate+"')]")).click();
-        LoadingWait.ExplicitWait(driver);
-        String actualPlate = driver.findElement(By.xpath("//div[contains(text(),'"+plate+"')]")).getText();
-        Assert.assertEquals(actualPlate.substring(0,8),plate);
+        //locate the first cell and click
+        WebElement firstRowPlate= driver.findElement(By.xpath("(//tbody//td)[1]"));
+        String expectedPlate= firstRowPlate.getText();
+        firstRowPlate.click();
 
-        expectedTitle = "14EF6562 Tera Hermiston I Nijat did this :)New Aimee 2009 silver compact,sedan - Car - Entities - System - Car - Entities - System";
-        actualTitle = driver.getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
-         */
-        // select and check/test the first car on table via its plate
-        WebElement firstRow_Column = driver.findElement(By.xpath("(//tbody//td)[1]"));
-        String firstRowPlate = firstRow_Column.getText();
-        firstRow_Column.click();
-        LoadingWait.ExplicitWait(driver);
+        //wait
+        wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
 
-        String actualPlate = driver.findElement(By.xpath("(//h5/../div/div)[1]")).getText();  //chil -- parent -- child
-        Assert.assertEquals(actualPlate,firstRowPlate);
+        //Verify system should display general information about the car
+        String actualPlate= driver.findElement(By.xpath("(//h5/../div/div)[1]")).getText();  // //chil -- parent -- child
+        Assert.assertEquals(actualPlate, expectedPlate);
 
         // and also Verify General info page
         String pageHead= driver.findElement(By.tagName("h5")).getText();
@@ -118,50 +116,48 @@ public class UserStory1_TestCase {
         //*****************  Verify AC-3  *****************
 
         // Click on add event button
-        //Thread.sleep(2000);
-        //driver.findElement(By.xpath("(//a[@href=\"javascript: void(0);\"])[2]")).click();
         driver.findElement(By.partialLinkText("Add Event")).click(); //ATTENTION, direk link ismi kullanma, boşluk vb var!!
-        LoadingWait.ExplicitWait(driver);
+        //driver.findElement(By.cssSelector(".btn.icons-holder-text.no-hash")).click();
 
-        // add event
-        String EventTitle = "Automation Event By Tlh";
-        driver.findElement(By.name("oro_calendar_event_form[title]")).sendKeys(EventTitle);
-        // switch iframe
+        //wait
+        wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
+
+        //enter input into  event title
+        String eventTitle= "group study title";
+        driver.findElement(By.name("oro_calendar_event_form[title]")).sendKeys(eventTitle);
+
+        //switch to iframe
         driver.switchTo().frame(0);
-        driver.findElement(By.id("tinymce")).sendKeys("FleetModule Vehicle Event/UserStory-1");
+        driver.findElement(By.id("tinymce")).sendKeys("This is group study description");
 
-        // switch back and click submit
+        //switch back to the main html page & click submit BUTTON
         driver.switchTo().defaultContent();
         driver.findElement(By.cssSelector("button[type='submit']")).click();
 
-        // add wait for visibilty of flash message
+        //wait
+        wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
+
+        // Verify flash message is displayed
         WebElement flashMessage= driver.findElement(By.xpath("//div[@class='flash-messages-holder']"));
-        WebDriverWait wait = new WebDriverWait(driver,10);
+
+        // add wait for visibilty of flash message
         wait.until(ExpectedConditions.visibilityOf(flashMessage));
-        // System.out.println("flash Message text: " +flashMessage.getText());
-        Assert.assertTrue(flashMessage.isDisplayed(), "Add event message is displayed");
+        System.out.println("flashMessage = " + flashMessage.getText());
 
-        //verify that event can see in activty tab
-        driver.navigate().refresh();  // after refrsh page we can see new event!!!!!!
-        LoadingWait.ExplicitWait(driver);
+        Assert.assertTrue(flashMessage.isDisplayed(), "add event message is displayed");
+
+        //Verify that truck driver can add Event and it should display under Activity tab and General
+
+        //refresh the page for the event to be displayed
+        driver.navigate().refresh();
+        wait.until(ExpectedConditions.invisibilityOfAllElements(loader));
+
         String actualEventTitle= driver.findElement(By.tagName("strong")).getText();
-        Assert.assertEquals(actualEventTitle,EventTitle);
+        Assert.assertEquals(actualEventTitle, eventTitle);
 
 
-
+        // to be continue insallah... (10/05/2020)
         //*****************  Verify AC-4  *****************
-
-        driver.navigate().back();
-        LoadingWait.ExplicitWait(driver);
-        driver.findElement(By.xpath("(//*[@class='fa-refresh'])[2]")).click();
-        LoadingWait.ExplicitWait(driver);
-        // bunun testi nasıl olur bi açıklama yok ! istersen settings den id ekle sonra restele eski haline döner
-
-
-
-
-
-
 
     }
 
