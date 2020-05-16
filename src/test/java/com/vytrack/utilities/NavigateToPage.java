308d4839aa7c97bd5039e2713f4c271f1230f47c
bahadirkani <bahadirbektas.gmail.com>
2020-05-16 17:24:01 +0100

    16/05 Group study (Utilities updated)

---
+++ src/test/java/com/vytrack/utilities/NavigateToPage.java
@@ -0,0 +1,28 @@
+package com.vytrack.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class NavigateToPage {
+
+    public static void navigateToModulePage (WebDriver driver, String moduleName, String pageName ){
+
+        WebElement module = driver.findElement(By.xpath("//a//span[contains(text(),'" + moduleName + "')]"));
+        WebElement page = driver.findElement(By.xpath("//span[.='" + pageName + "']"));
+
+        Actions action = new Actions(driver);
+        action.moveToElement(module).perform();
+
+        page.click();
+
+        LoadingWaits.expWait(driver);
+
+
+
+
+
+
+    }
+}
