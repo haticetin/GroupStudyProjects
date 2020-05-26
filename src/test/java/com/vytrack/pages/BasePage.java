7b787ead5ab01ae8aeabeea65b15f2b49b36538e
EmineBalci <cangunes156@gmail.com>
2020-05-26 15:30:44 +0200

    16.05 Group Study Smoke-MenuOptionsTestUpdate

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public abstract class BasePage {
+public abstract class giBasePage {
 
     @FindBy(css = "div[class='loader-mask shown']")
     @CacheLookup
